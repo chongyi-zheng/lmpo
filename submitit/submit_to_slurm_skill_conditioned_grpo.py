7f95bad9ad3b8eff1eafc281d3bed2e84a2d8341
chongyi-zheng <chongyiz@andrew.cmu.edu>
2025-08-03 15:11:19 -0400

    update submitit/submit_to_slurm_skill_conditioned_grpo.py

--- submitit/submit_to_slurm_skill_conditioned_grpo.py
+++ submitit/submit_to_slurm_skill_conditioned_grpo.py
@@ -61,7 +61,7 @@ def main():
     with executor.batch():  # job array
         for env_name in ['countdown']:
             for num_steps in [160]:
-                for inference_batch_per_device in [20]:
+                for inference_batch_per_device in [18]:
                     for ppo_minibatch in [64]:
                         for entropy_coef in [0.001, 0.0001, 0.0]:
                             for seed in [10, 20]:
